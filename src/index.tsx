a5bcb692c6624d781242ad50be1153f53ab99df4
Monika <zubovdns@icloud.com>
2024-07-24 14:50:00 +0300

    feat: added global style normalizer

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from '@components/App';
+import { GlobalStyles } from '@styles/GlobalStyles';
+
+const MainComponent = () => (
+	<>
+		<GlobalStyles />
+		<App />
+	</>
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<MainComponent />
 		</BrowserRouter>
 	</React.StrictMode>
 );
