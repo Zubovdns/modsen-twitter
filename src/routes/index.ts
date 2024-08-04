cb1fac5368163f0fde5a1bb7bf14e20955552c85
Monika <zubovdns@icloud.com>
2024-08-04 10:30:57 +0530

    style: delete unused route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,6 @@ const SignUp = lazy(() => import('@pages/SignUp'));
 export const HOME_ROUTE = '/home';
 export const SIGN_UP_ROUTE = '/sign_up';
 export const REGISTER_ROUTE = '/register';
-export const REGISTER_PASSWORD_ROUTE = '/register_password';
 export const LOGIN_ROUTE = '/login';
 
 export const authenticatedRoutes = [{ path: HOME_ROUTE, Page: Home }];
@@ -16,6 +15,5 @@ export const authenticatedRoutes = [{ path: HOME_ROUTE, Page: Home }];
 export const unauthenticatedRoutes = [
 	{ path: SIGN_UP_ROUTE, Page: SignUp },
 	{ path: REGISTER_ROUTE, Page: Register },
-	{ path: REGISTER_PASSWORD_ROUTE, Page: 'div' },
 	{ path: LOGIN_ROUTE, Page: Login },
 ];
