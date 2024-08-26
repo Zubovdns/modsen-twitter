ef40842062d1bd9186af332973b51baf6af90aed
Monika <zubovdns@icloud.com>
2024-08-26 19:30:01 +0300

    feat: added redirect from / to /home

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,10 @@
 import { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { authenticatedRoutes, unauthenticatedRoutes } from '@src/routes';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+	authenticatedRoutes,
+	HOME_ROUTE,
+	unauthenticatedRoutes,
+} from '@src/routes';
 
 import { Loader } from '../Loader';
 import { MainLayout } from '../MainLayout';
@@ -10,6 +14,7 @@ import { UnauthenticatedRoute } from '../UnauthenticatedRoute';
 export const App = () => (
 	<Suspense fallback={<Loader />}>
 		<Routes>
+			<Route path='/' element={<Navigate to={HOME_ROUTE} />} />
 			{authenticatedRoutes.map(({ path, Page }) => (
 				<Route
 					key={path}
