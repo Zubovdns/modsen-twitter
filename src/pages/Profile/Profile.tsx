5c45c17a16d4f28f5647780a704f044bdc9c6165
Monika <zubovdns@icloud.com>
2024-08-28 10:51:32 +0300

    feat: added static header and tweet input for profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,23 +1,88 @@
+import { useState } from 'react';
 import { SearchPanel } from '@src/components/SearchPanel';
 import { ThemeSwitcher } from '@src/components/ThemeSwitcher';
 
+import { TweetInput } from '../Home/TweetInput';
+
 import {
+	BannerImage,
+	BannerImageContainer,
+	BannerWrapper,
+	Bio,
+	EditButton,
+	EditButtonContainer,
+	FollowInfo,
 	HeaderContainer,
+	Info,
+	Name,
+	NumberOfPosts,
 	ProfileContainer,
+	ProfileHeaderContainer,
+	ProfileImage,
+	ProfileImageContainer,
 	ProfileWrapper,
+	TextContainer,
 	Title,
+	TitleContainer,
+	Username,
+	UsernameContainer,
 } from './styled';
 
-export const Profile = () => (
-	<>
-		<ProfileWrapper>
-			<ProfileContainer>
-				<HeaderContainer>
-					<Title>Profile</Title>
-					<ThemeSwitcher />
-				</HeaderContainer>
-			</ProfileContainer>
-		</ProfileWrapper>
-		<SearchPanel />
-	</>
-);
+export const Profile = () => {
+	const [bannerError, setBannerError] = useState(false);
+	const [profileError, setProfileError] = useState(false);
+
+	return (
+		<>
+			<ProfileWrapper>
+				<ProfileContainer>
+					<HeaderContainer>
+						<TitleContainer>
+							<Title>Profile</Title>
+							<NumberOfPosts>20 Tweets</NumberOfPosts>
+						</TitleContainer>
+						<ThemeSwitcher />
+					</HeaderContainer>
+					<ProfileHeaderContainer>
+						<BannerWrapper>
+							<BannerImageContainer>
+								{!bannerError ? (
+									<BannerImage
+										src='https://via.placeholder.com/350x150'
+										alt='Banner'
+										onError={() => setBannerError(true)}
+									/>
+								) : null}
+							</BannerImageContainer>
+
+							<ProfileImageContainer>
+								{!profileError ? (
+									<ProfileImage
+										src='https://via.placeholder.com/130'
+										alt='Profile'
+										onError={() => setProfileError(true)}
+									/>
+								) : null}
+							</ProfileImageContainer>
+						</BannerWrapper>
+						<EditButtonContainer>
+							<EditButton>Edit profile</EditButton>
+						</EditButtonContainer>
+
+						<TextContainer>
+							<UsernameContainer>
+								<Name>Monika</Name>
+								<Username>@zubovdns</Username>
+							</UsernameContainer>
+							<Bio>Love Is War</Bio>
+							<Info>Joined September 2019</Info>
+							<FollowInfo>58 Following | 7 Followers</FollowInfo>
+						</TextContainer>
+					</ProfileHeaderContainer>
+					<TweetInput avatarUrl='https://via.placeholder.com/130' />
+				</ProfileContainer>
+			</ProfileWrapper>
+			<SearchPanel />
+		</>
+	);
+};
--- src/pages/Profile/styled.ts
+++ src/pages/Profile/styled.ts
@@ -27,6 +27,132 @@ export const HeaderContainer = styled.header`
 	justify-content: space-between;
 `;
 
+export const TitleContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+`;
+
 export const Title = styled.h1`
 	font-size: 20px;
+`;
+
+export const NumberOfPosts = styled.p`
+	color: #536471;
+	font-size: 13px;
+`;
+
+export const ProfileHeaderContainer = styled.div`
+	width: 100%;
+`;
+
+export const BannerWrapper = styled.div`
+	position: relative;
+	width: 100%;
+	height: 200px;
+`;
+
+export const BannerImageContainer = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: #ccc;
+	overflow: hidden;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const BannerImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+
+export const ProfileImageContainer = styled.div`
+	position: absolute;
+	bottom: -60px;
+	left: 20px;
+	border-radius: 50%;
+	width: 130px;
+	height: 130px;
+	background-color: #ccc;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	overflow: hidden;
+	border: 3px solid #fff;
+`;
+
+export const ProfileImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+
+export const EditButtonContainer = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: flex-end;
+	align-items: flex-start;
+
+	padding: 10px 10px;
+`;
+
+export const EditButton = styled.button`
+	padding: 6px 14px;
+	font-size: 16px;
+	left: 1000px;
+	color: #333;
+	font-weight: bold;
+
+	background-color: inherit;
+	border: 1px solid #ccc;
+	border-radius: 30px;
+	cursor: pointer;
+
+	&:hover {
+		background-color: #d4d9dd;
+	}
+`;
+
+export const TextContainer = styled.div`
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+
+	gap: 10px;
+
+	padding: 10px 20px;
+`;
+
+export const UsernameContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+`;
+
+export const Name = styled.h2`
+	font-size: 20px;
+`;
+
+export const Username = styled.p`
+	font-size: 15px;
+	color: #536471;
+`;
+
+export const Bio = styled.p`
+	font-size: 15px;
+`;
+
+export const Info = styled.p`
+	font-size: 15px;
+	color: #aaa;
+`;
+
+export const FollowInfo = styled.p`
+	font-size: 14px;
+	color: #333;
 `;
