ed50a32a74b2033249f20c2c70e045725e2e25c2
Monika <zubovdns@icloud.com>
2024-08-28 11:26:47 +0300

    feat: fix navbar for new routing

--- src/components/NavBar/Profile/Profile.tsx
+++ src/components/NavBar/Profile/Profile.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ExitIcon from '@assets/icons/NavBar/ExitIcon.svg';
 import { useNotification } from '@hooks/useNotification';
 import { auth, db } from '@src/firebase';
+import { SIGN_UP_ROUTE } from '@src/routes';
 import { signOut } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 
@@ -18,8 +20,10 @@ import {
 export const Profile = () => {
 	const [showNotification, NotificationComponent] = useNotification();
 	const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+	const [isAuth, setIsAuth] = useState(false);
 	const [name, setName] = useState<string | null>(null);
 	const [loginName, setLoginName] = useState<string | null>(null);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchUserProfile = async () => {
@@ -34,10 +38,12 @@ export const Profile = () => {
 						setAvatarUrl(userData.profile_image);
 						setName(userData.name);
 						setLoginName(userData.login_name);
+						setIsAuth(true);
 					}
 				}
 			} catch (error) {
 				showNotification('Failed to load user data.');
+				setIsAuth(false);
 			}
 		};
 
@@ -46,12 +52,13 @@ export const Profile = () => {
 
 	const handleLogOut = async () => {
 		try {
-			await signOut(auth);
+			await signOut(auth).then(() => navigate(SIGN_UP_ROUTE));
 		} catch (error) {
 			showNotification(LOG_OUT_ERROR_MESSAGE);
 		}
 	};
 
+	if (!isAuth) return null;
 	return (
 		<>
 			<ProfileWrapper onClick={handleLogOut}>
