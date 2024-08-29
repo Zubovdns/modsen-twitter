bb2b49c71d4880a4c9688221f41e4fb791134bf6
Monika <zubovdns@icloud.com>
2024-08-29 13:09:28 +0300

    feat: add constants for static search

---
+++ src/components/SearchPanel/SearchBar/constants.ts
@@ -0,0 +1,27 @@
+export const NO_RESULT = "We can't find anything matching your request.";
+
+export const mockResults = [
+	{
+		name: 'John Doe',
+		loginName: 'johndoe',
+		avatarUrl: '',
+	},
+	{
+		name: 'Jane Smith',
+		loginName: 'janesmith',
+		avatarUrl:
+			'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+	},
+	{
+		name: 'Bob',
+		loginName: 'bobby',
+		avatarUrl:
+			'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+	},
+	{
+		name: 'Lara Croft',
+		loginName: 'lsde',
+		avatarUrl:
+			'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+	},
+];
