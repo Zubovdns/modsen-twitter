627ec740efd6d63f756af0eb784f44075a09adf2
Monika <zubovdns@icloud.com>
2024-08-29 13:10:08 +0300

    feat: implement search item and static search logic

--- src/components/SearchPanel/SearchBar/SearchBar.tsx
+++ src/components/SearchPanel/SearchBar/SearchBar.tsx
@@ -1,10 +1,76 @@
+import { useEffect, useRef, useState } from 'react';
 import SearchIcon from '@assets/icons/SearchPanel/SearchIcon.svg';
 
-import { SearchContainer, SearchImage, SearchInput } from './styled';
+import { mockResults, NO_RESULT } from './constants';
+import { SearchItem } from './SearchItem';
+import {
+	DropdownList,
+	EmptyMessage,
+	SearchContainer,
+	SearchImage,
+	SearchInput,
+} from './styled';
+import { SearchItemType } from './types';
 
-export const SearchBar = () => (
-	<SearchContainer>
-		<SearchImage src={SearchIcon} />
-		<SearchInput type='text' placeholder='Поиск' />
-	</SearchContainer>
-);
+export const SearchBar = () => {
+	const [isFocused, setIsFocused] = useState(false);
+	const [searchResults, setSearchResults] =
+		useState<SearchItemType[]>(mockResults);
+
+	const searchRef = useRef<HTMLDivElement>(null);
+
+	const handleFocus = () => {
+		setIsFocused(true);
+	};
+
+	const handleBlur = () => {
+		setTimeout(() => setIsFocused(false), 200);
+	};
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (
+			searchRef.current &&
+			!searchRef.current.contains(event.target as Node)
+		) {
+			setIsFocused(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
+	return (
+		<SearchContainer ref={searchRef}>
+			<SearchImage src={SearchIcon} />
+			<SearchInput
+				type='text'
+				placeholder='Поиск'
+				onFocus={handleFocus}
+				onBlur={handleBlur}
+			/>
+			{isFocused && (
+				<DropdownList>
+					{searchResults.length === 0 ? (
+						<EmptyMessage>{NO_RESULT}</EmptyMessage>
+					) : (
+						searchResults
+							.slice(0, 9)
+							.map(({ avatarUrl, name, loginName }) => (
+								<SearchItem
+									key={loginName}
+									avatarUrl={avatarUrl}
+									name={name}
+									loginName={loginName}
+								/>
+							))
+					)}
+				</DropdownList>
+			)}
+		</SearchContainer>
+	);
+};
--- src/components/SearchPanel/SearchBar/styled.ts
+++ src/components/SearchPanel/SearchBar/styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const SearchContainer = styled.div`
+	position: relative;
 	display: flex;
 	align-items: center;
 	background-color: #f1f4f6;
@@ -26,4 +27,67 @@ export const SearchInput = styled.input`
 	&::placeholder {
 		color: #6e7a82;
 	}
+`;
+
+export const DropdownList = styled.div`
+	position: absolute;
+	top: 100%;
+	left: 0;
+	right: 0;
+	background-color: white;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	border-radius: 10px;
+	margin-top: 8px;
+	max-height: 300px;
+	overflow-y: auto;
+	z-index: 10;
+`;
+
+export const DropdownItem = styled.div`
+	display: flex;
+	align-items: center;
+	padding: 10px;
+	cursor: pointer;
+	transition: background-color 0.2s;
+
+	&:hover {
+		background-color: #f1f4f6;
+	}
+`;
+
+export const ItemImageContainer = styled.div`
+	border-radius: 50%;
+	width: 40px;
+	height: 40px;
+	margin-right: 10px;
+
+	background-color: #ccc;
+	overflow: hidden;
+`;
+
+export const ItemImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+
+export const ItemTextContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const ItemName = styled.span`
+	font-size: 16px;
+	font-weight: bold;
+`;
+
+export const ItemLogin = styled.span`
+	font-size: 14px;
+	color: #6e7a82;
+`;
+
+export const EmptyMessage = styled.div`
+	padding: 10px;
+	text-align: center;
+	color: #6e7a82;
 `;
