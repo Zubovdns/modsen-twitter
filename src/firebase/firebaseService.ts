055e87889f963957aa0b0354f5443b17b3a2e378
Monika <zubovdns@icloud.com>
2024-09-02 15:03:13 +0300

    feat: added auth function

---
+++ src/firebase/firebaseService.ts
@@ -0,0 +1,32 @@
+import { doc, getDoc } from 'firebase/firestore';
+
+import { auth, db } from '.';
+
+export interface UserData {
+	name: string;
+	email: string;
+	followers: string[];
+	following: string[];
+	login_name: string;
+	birth_date: string | null;
+	phone_number: string | null;
+	profile_image: string | null;
+	background_profile_image: string | null;
+}
+
+export const getUserData = async (): Promise<UserData | null> => {
+	const user = auth.currentUser;
+
+	if (!user) {
+		throw new Error('User is not authenticated');
+	}
+
+	const userDocRef = doc(db, 'users', user.uid);
+	const userDocSnap = await getDoc(userDocRef);
+
+	if (userDocSnap.exists()) {
+		return userDocSnap.data() as UserData;
+	} else {
+		throw new Error('User document not found');
+	}
+};
