029517308fa4abba96e22cfdd756bc67407be6bf
Monika <zubovdns@icloud.com>
2024-09-02 15:55:42 +0300

    style: move UserData to interface

--- src/firebase/firebaseService.ts
+++ src/firebase/firebaseService.ts
@@ -1,18 +1,8 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { UserData } from '@src/interfaces/user';
+import { signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
-import { auth, db } from '.';
-
-export interface UserData {
-	name: string;
-	email: string;
-	followers: string[];
-	following: string[];
-	login_name: string;
-	birth_date: string | null;
-	phone_number: string | null;
-	profile_image: string | null;
-	background_profile_image: string | null;
-}
+import { auth, db, googleProvider } from '.';
 
 export const getUserData = async (): Promise<UserData | null> => {
 	const user = auth.currentUser;
@@ -28,5 +18,33 @@ export const getUserData = async (): Promise<UserData | null> => {
 		return userDocSnap.data() as UserData;
 	} else {
 		throw new Error('User document not found');
+	}
+};
+
+export const loginViaGoogle = async (): Promise<void> => {
+	try {
+		const result = await signInWithPopup(auth, googleProvider);
+		const user = result.user;
+
+		const userDocRef = doc(db, 'users', user.uid);
+		const userDocSnap = await getDoc(userDocRef);
+
+		if (!userDocSnap.exists()) {
+			const userData: UserData = {
+				name: user.displayName || '',
+				phone_number: user.phoneNumber || '',
+				birth_date: null,
+				email: user.email || '',
+				profile_image: null,
+				background_profile_image: null,
+				login_name: user.uid,
+				followers: [],
+				following: [],
+			};
+
+			await setDoc(userDocRef, userData);
+		}
+	} catch (error) {
+		throw new Error('Cant login via google');
 	}
 };
---
+++ src/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface UserData {
+	name: string;
+	email: string;
+	followers: string[];
+	following: string[];
+	login_name: string;
+	birth_date: string | null;
+	phone_number: string | null;
+	profile_image: string | null;
+	background_profile_image: string | null;
+}
