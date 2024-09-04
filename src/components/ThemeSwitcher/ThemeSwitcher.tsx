c5d11388ff5e9296ac59e67f7c6e6e2e592f8582
Monika <zubovdns@icloud.com>
2024-09-04 09:02:58 +0300

    feat: add theme change

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
+import { DARK, LIGHT } from '@constants/theme';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
+import { themeMode } from '@store/selectors/theme';
+import { setTheme } from '@store/slices/theme';
 
 import { Switch } from './styled';
 
 export const ThemeSwitcher = () => {
-	const [checked, setChecked] = useState(false);
+	const dispatch = useAppDispatch();
+	const theme = useAppSelector(themeMode);
+
+	const [checked, setChecked] = useState(theme === DARK);
+
 	const handleThemeChange = () => {
+		dispatch(setTheme(checked ? LIGHT : DARK));
 		setChecked((prev) => !prev);
 	};
+
 	return <Switch checked={checked} onClick={handleThemeChange} />;
 };
