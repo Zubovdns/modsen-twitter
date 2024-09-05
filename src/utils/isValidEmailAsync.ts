e94473f0629e39ec3284f1a310554b811a9bdf2e
Monika <zubovdns@icloud.com>
2024-09-05 13:24:30 +0300

    style: decompose check email into a function

--- src/utils/isValidEmailAsync.ts
+++ src/utils/isValidEmailAsync.ts
@@ -1,20 +1,6 @@
-import { db } from '@api/firebase/config';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { isFieldWithDataExistsInCollection } from '@api/firebase/firestore';
 
 import { isValidEmail } from './isValidEmail';
-
-export const isEmailExistsInFirestore = async (
-	email: string
-): Promise<boolean> => {
-	try {
-		const q = query(collection(db, 'users'), where('email', '==', email));
-		const querySnapshot = await getDocs(q);
-		return !querySnapshot.empty;
-	} catch (error) {
-		console.error('Error checking email in Firestore: ', error);
-		throw new Error('Error checking email in Firestore');
-	}
-};
 
 export const isValidEmailAsync = async (value: string) => {
 	const syncValidationCheck = isValidEmail(value);
@@ -23,7 +9,11 @@ export const isValidEmailAsync = async (value: string) => {
 	}
 
 	try {
-		const emailExists = await isEmailExistsInFirestore(value);
+		const emailExists = await isFieldWithDataExistsInCollection(
+			'users',
+			'email',
+			value
+		);
 		if (emailExists) {
 			return 'Email already in use';
 		}
