38a90252c383d0312f986dcd2d152f2777accf95
Monika <zubovdns@icloud.com>
2024-09-16 11:41:15 +0300

    refactor: add boolean conversion

--- src/components/NavBar/MiniProfile/Avatar/Avatar.tsx
+++ src/components/NavBar/MiniProfile/Avatar/Avatar.tsx
@@ -3,6 +3,6 @@ import { AvatarProps } from './types';
 
 export const Avatar = ({ src, alt }: AvatarProps) => (
 	<AvatarContainer>
-		{src && <AvatarImage src={src} alt={alt} />}
+		{!!src && <AvatarImage src={src} alt={alt} />}
 	</AvatarContainer>
 );
